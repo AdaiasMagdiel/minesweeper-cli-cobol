1a542f58c9de21ea3d61d4c644afc8d12772129f
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-07-20 17:53:56 -0300

    feat: implement game menu and user choice handling - add display banner and menu functionality - introduce user choice variable and input handling - implement game start and exit logic based on user choice - modify board display and initialization to accommodate new features

--- MINESWPR.cbl
+++ MINESWPR.cbl
@@ -3,6 +3,7 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
+       01 WS-USER-CHOICE PIC X.
        01 WS-BOARD OCCURS 10 TIMES INDEXED BY WS-ROW-IDX.
            05 WS-BOARD-ROW OCCURS 10 TIMES INDEXED BY WS-COL-IDX.
                10 WS-CELL-STATE PIC X.
@@ -13,15 +14,51 @@
            05 WS-ROW-DISPLAY PIC Z9.
 
        PROCEDURE DIVISION.
+       PERFORM DISPLAY-BANNER.
+       PERFORM DISPLAY-MENU.
 
-       PERFORM CLEAR-BOARD.
-
-       SET CELL-BOMB  (4, 5) TO TRUE.
-       SET CELL-EMPTY (6, 2) TO TRUE.
-
-       PERFORM DISPLAY-BOARD.
+       DISPLAY "THE GAME!".
 
        STOP RUN.
+
+       DISPLAY-BANNER.
+           DISPLAY "                ___ __  __  __  _".
+           DISPLAY "               / _//__\|  \/__\| |".
+           DISPLAY "              | \_| \/ | -< \/ | |_".
+           DISPLAY "               \__/\__/|__/\__/|___|".
+           DISPLAY
+               " __ __ _ __  _ ___  __  _   _  ___ ___ ___ ___ ___ ".
+           DISPLAY
+               "|  V  | |  \| | __/' _/| | | || __| __| _,\ __| _ \".
+           DISPLAY
+               "| \_/ | | | ' | _|`._`.| 'V' || _|| _|| v_/ _|| v /".
+           DISPLAY
+               "|_| |_|_|_|\__|___|___/!_/ \_!|___|___|_| |___|_|_\".
+
+           DISPLAY " ".
+           DISPLAY " ".
+           
+       DISPLAY-MENU.
+           DISPLAY "1. PLAY".
+           DISPLAY "2. EXIT".
+           DISPLAY " ".
+
+           DISPLAY "ENTER A OPTION: " WITH NO ADVANCING.
+           
+           PERFORM UNTIL 1 = 0
+               ACCEPT WS-USER-CHOICE
+
+               EVALUATE WS-USER-CHOICE
+                   WHEN 1
+                       EXIT PERFORM
+                   WHEN 2
+                       STOP RUN
+                   WHEN OTHER
+                       DISPLAY
+                           "PLEASE, CHOICE A VALID OPTION (1 OR 2): "
+                           WITH NO ADVANCING
+               END-EVALUATE
+           END-PERFORM.
 
        CLEAR-BOARD.
            PERFORM VARYING WS-ROW-IDX FROM 1 BY 1 UNTIL WS-ROW-IDX > 10
